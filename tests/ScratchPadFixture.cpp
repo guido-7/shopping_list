c2b159820427141309881d8ef2d5b6befb4c904c
Guido Rossi <guidor0033@gmail.com>
2023-08-10 19:11:22 +0200

    Add ScratchPadSuite at ScratchPadFixture.cpp.

--- tests/ScratchPadFixture.cpp
+++ tests/ScratchPadFixture.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Guido Rossi on 10/08/2023.
 //
+#include "gtest/gtest.h"
+#include "../ScratchPad.h"
+
+class ScratchPadSuite : public ::testing::Test {
+
+protected:
+    ScratchPadSuite() : sbuf{nullptr} {}
+    ~ScratchPadSuite() override = default;
+
+    virtual void SetUp() {
+        sbuf = std::cout.rdbuf();
+        std::cout.rdbuf(buffer.rdbuf());
+    }
+    void TearDown() override {
+        std::cout.rdbuf(sbuf);
+        sbuf = nullptr;
+    }
+
+    ScratchPad c;
+    std::stringstream buffer{};
+    std::streambuf *sbuf;
+};
