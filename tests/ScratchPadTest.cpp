3c27b8cf6e4abd6f9b4b1c4c0cee106d85d93d3d
Guido Rossi <guidor0033@gmail.com>
2023-08-11 11:46:10 +0200

    Add TestAddList at ScratchPadTest.

--- tests/ScratchPadTest.cpp
+++ tests/ScratchPadTest.cpp
@@ -10,3 +10,13 @@ TEST(ScratchPad, DefaultConstructor) {
     ASSERT_EQ(0, c.size());
     ASSERT_EQ(0, c.getIndexListOpen());
 }
+
+TEST(ScratchPad, TestAddList) {
+    ScratchPad c;
+    c.addList("List1");
+    ASSERT_EQ(1, c.size());
+    ASSERT_EQ(2, c.getIndexListOpen());
+    c.addList("List2");
+    ASSERT_EQ(2, c.size());
+    ASSERT_EQ(3, c.getIndexListOpen());
+}
