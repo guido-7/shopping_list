320b1a82162f98095920597cb707d6b262c616c3
Guido Rossi <guidor0033@gmail.com>
2023-08-14 12:42:46 +0200

    Add TestCopyConstructor in ItemTest.

--- tests/ItemTest.cpp
+++ tests/ItemTest.cpp
@@ -10,3 +10,11 @@ TEST(Item, DefaultConstructor) {
     ASSERT_EQ(1, i.getQuantity());
     ASSERT_FALSE(i.isTaken());
 }
+
+TEST(Item, TestCopyConstructor) {
+    Item i("Item1", 2, true);
+    Item j(i);
+    ASSERT_EQ(i.getName(), j.getName());
+    ASSERT_EQ(i.getQuantity(), j.getQuantity());
+    ASSERT_EQ(i.isTaken(), j.isTaken());
+}
