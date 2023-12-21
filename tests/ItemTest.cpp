405d3e7cbbc63c6d07e68ecfc3441029333502d7
Guido Rossi <guidor0033@gmail.com>
2023-12-21 17:48:11 +0100

    Fix ItemTest

--- tests/ItemTest.cpp
+++ tests/ItemTest.cpp
@@ -5,16 +5,14 @@
 #include "../List.h"
 
 TEST(Item, DefaultConstructor) {
-    auto *L = new List("List1");
-    Item i(L, "Item1");
+    Item i("Item1");
     ASSERT_EQ("Item1", i.getName());
     ASSERT_EQ(1, i.getQuantity());
     ASSERT_FALSE(i.isTaken());
 }
 
 TEST(Item, TestCopyConstructor) {
-    auto *L = new List("List1");
-    Item i(L, "Item1", 2, true);
+    Item i("Item1", 2, true);
     Item j(i);
     ASSERT_EQ(i.getName(), j.getName());
     ASSERT_EQ(i.getQuantity(), j.getQuantity());
@@ -22,9 +20,8 @@ TEST(Item, TestCopyConstructor) {
 }
 
 TEST(Item, TestAssignmentOperator) {
-    auto *L = new List("List1");
-    Item i(L, "Item1", 2, true);
-    Item j(L, "Item2");
+    Item i("Item1", 2, true);
+    Item j("Item2");
     j = i;
     ASSERT_EQ(i.getName(), j.getName());
     ASSERT_EQ(i.getQuantity(), j.getQuantity());
@@ -32,22 +29,19 @@ TEST(Item, TestAssignmentOperator) {
 }
 
 TEST(Item, TestSetName) {
-    auto *L = new List("List1");
-    Item i(L, "Item1");
+    Item i("Item1");
     i.setName("Item2");
     ASSERT_EQ("Item2", i.getName());
 }
 
 TEST(Item, TestSetQuantity) {
-    auto *L = new List("List1");
-    Item i(L, "Item1");
+    Item i("Item1");
     i.setQuantity(2);
     ASSERT_EQ(2, i.getQuantity());
 }
 
 TEST(Item, TestSetTaken) {
-    auto *L = new List("List1");
-    Item i(L, "Item1");
+    Item i("Item1");
     i.setTaken(true);
     ASSERT_TRUE(i.isTaken());
 }
